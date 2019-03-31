20697888d84cd0a307e3cd48dfebd8ce50ba664b
Nick <walkmanaly@gmail.com>
2019-03-31 23:15:54 +0800

    day2 code of Sets

--- 100daysOfSwift.playground/Contents.swift
+++ 100daysOfSwift.playground/Contents.swift
@@ -15,3 +15,11 @@ food[2]
 let intArr: [Int] = [1, 2, 3, 4, 5]
 
 intArr[4]
+
+
+// Sets
+// 与数组区别：1、无序 2、值不重复
+
+let color1 = Set(["red", "blue", "orange"])
+
+let color2 = Set(["red", "blue", "orange", "red", "blue"])
