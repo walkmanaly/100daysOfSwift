ef92727624fa0b379e33d46ba3974ce0cebe886f
Nick <walkmanaly@gmail.com>
2019-03-31 23:06:11 +0800

    day2 code of array

---
+++ 100daysOfSwift.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Array
+
+let orange = "orange"
+let piapple = "piapple"
+let apple = "apple"
+let mango = "mango"
+
+let food = [orange, piapple, apple, mango]
+food[2]
+
+let intArr: [Int] = [1, 2, 3, 4, 5]
+
+intArr[4]
