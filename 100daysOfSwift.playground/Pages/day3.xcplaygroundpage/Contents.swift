367aa762ee264515751ce55a90ec2de64e3f5915
Nick <walkmanaly@gmail.com>
2019-04-01 23:11:26 +0800

    基础运算符

---
+++ 100daysOfSwift.playground/Pages/day3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 基础运算符
+let firstScore = 13
+let secondScore = 2
+
+let sum = firstScore + secondScore
+let diffent = firstScore - secondScore
+
+let mutiple = firstScore * secondScore
+let divid = firstScore / secondScore
+
+let mord = firstScore % secondScore
+
+// 重载运算符
+
+let firstName = "Nick "
+let lastName = "Stark"
+
+let fullName = firstName + lastName
+
+let names1 = ["rose", "jack"]
+let names2 = ["arya", "branden"]
+
+let allName = names1 + names2
+
+
+//: [Next](@next)
