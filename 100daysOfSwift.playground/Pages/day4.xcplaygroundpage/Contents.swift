fb8dfe0e2da165aa5b1881048d19e040ea45c2af
Nick <walkmanaly@gmail.com>
2019-04-02 23:12:43 +0800

    day4 for循环

---
+++ 100daysOfSwift.playground/Pages/day4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// loop 循环
+let count = 1...10
+for number in count {
+    print("Number loop \(number)")
+}
+
+let abulm = ["james blumt", "talor swift", "coldplay"]
+for singer in abulm {
+    print("\(singer) is on apple music")
+}
+
+// _可以省略参数
+for _ in 1..<6 {
+    print("let's loop")
+}
+
+//: [Next](@next)
