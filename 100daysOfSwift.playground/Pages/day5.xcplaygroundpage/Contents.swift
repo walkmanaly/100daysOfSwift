cd4319cc6edfbcb065bcc8d7f55ca6959b5bc134
Nick <walkmanaly@gmail.com>
2019-04-03 22:32:08 +0800

    day5 方法func

---
+++ 100daysOfSwift.playground/Pages/day5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "function"
+
+// 方法
+func runMyApp() {
+    let message = """
+Hello! This is my day5 learned swift!
+"""
+    print(message)
+}
+
+runMyApp()
+
+//: [Next](@next)
