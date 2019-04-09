ad166e22cc7a14be0afd013277fc944e071b2e30
Nick <walkmanaly@gmail.com>
2019-04-09 22:57:17 +0800

    day12 unwrapping optional

--- 100daysOfSwift.playground/Pages/day12.xcplaygroundpage/Contents.swift
+++ 100daysOfSwift.playground/Pages/day12.xcplaygroundpage/Contents.swift
@@ -2,10 +2,24 @@
 
 import Foundation
 
-// optional
+// 1、optional
 
 var age: Int? = nil
 age = 18
+
+// 2、使用optional的值：使用if let gf如果有值使用，没有就走else
+var name: String? = nil
+
+if let unwrapping = name {
+    print("name is \(unwrapping)")
+} else {
+    print("unwrapping error")
+}
+
+
+
+
+
 
 
 //: [Next](@next)
