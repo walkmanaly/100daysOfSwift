893bd8d8c03a0cf255b835267c0090cc151dd475
Nick <walkmanaly@gmail.com>
2019-04-12 00:44:34 +0800

    day14

---
+++ 100daysOfSwift.playground/Pages/day14.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// review
+
+// 1、function
+// 2、optional
+    /*
+        optional值传递或返回方法中时，是语义确定的。
+        访问optional的值有以下方法：
+             1、if let value = value {
+             }
+             2、optional chianing
+             3、nil coalescing operater
+    */
+// 3、enum
+/*
+    1、switch enum时，必须处理所有的情况，否则编译不通过（swift安全的原因之一）
+    2、enum可以在case中关联值
+    3、enum可以指定类型，使用rawValue
+ */
+// 4、struct
+// 5、class
+/*
+    1、There are three solutions: make the two values optional strings, give them default values, or write our own initializer
+    2、可继承，子类初始化方法先初始化属性，再调父类初始化方法
+    3、方法重写
+*/
+
+//: [Next](@next)
