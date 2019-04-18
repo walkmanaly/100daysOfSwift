75a2d406132d53be9ddfe2c68228c1a8b371261a
Nick <walkmanaly@gmail.com>
2019-04-18 23:28:25 +0800

    day23 initial project3

---
+++ Project3/Project3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Project3
+//
+//  Created by Nick on 2019/4/18.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
