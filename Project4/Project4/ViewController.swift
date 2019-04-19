d96a00dc299e066102c0057591945efe46fbb2f8
Nick <walkmanaly@gmail.com>
2019-04-19 23:10:01 +0800

    day24 project4 initialize

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Nick on 2019/4/19.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
