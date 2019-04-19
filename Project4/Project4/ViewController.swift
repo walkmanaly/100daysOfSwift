9727bc0711b2bdc1fa23fbecf87643f7fff39acc
Nick <walkmanaly@gmail.com>
2019-04-19 23:53:33 +0800

    day24 wkwebview load diffrent website

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -22,9 +22,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openClicked))
+        
+        let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openClicked() {
+        let ac = UIAlertController(title: "Website", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: chooseWebsite))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: chooseWebsite))
+        ac.addAction(UIAlertAction(title: "cancal", style: .cancel, handler: nil))
+        popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func chooseWebsite(action: UIAlertAction) {
+        guard let actionTitle = action.title else {
+            return
+        }
+        guard let url = URL(string: "https://" + actionTitle) else {
+            return
+        }
+        webView.load(URLRequest(url: url))
     }
 }
 
