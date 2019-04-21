d74b9884dec7b41cfa820340e1ded9537b7b284c
Nick <walkmanaly@gmail.com>
2019-04-21 23:50:58 +0800

    day27 Project5 set up

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Nick on 2019/4/21.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
