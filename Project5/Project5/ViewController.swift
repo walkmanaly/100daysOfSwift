dd957c1895775aa540806ed39cc7a07505c5dd45
Nick <walkmanaly@gmail.com>
2019-04-22 00:11:56 +0800

    day27 project5 part1

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var allWords = [String]()
+    var useWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let allWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let allWordsStr = try? String(contentsOf: allWordsURL) {
+                allWords = allWordsStr.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["fail"]
+        }
+        
+        startGame()
+        
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        useWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return useWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "word", for: indexPath)
+        cell.textLabel?.text = useWords[indexPath.row]
+        return cell
+    }
 
 }
 
