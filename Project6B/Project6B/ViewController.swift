a74f51e59c57a78ef203fa41fc7bfb31aa8e8c31
Nick <walkmanaly@gmail.com>
2019-04-24 23:24:56 +0800

    day30 project6b init

---
+++ Project6B/Project6B/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project6B
+//
+//  Created by Nick on 2019/4/24.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
