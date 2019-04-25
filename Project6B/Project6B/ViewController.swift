f723575b534ea2fd246441de00f3c0b41bdb13d5
Nick <walkmanaly@gmail.com>
2019-04-25 23:16:59 +0800

    day31 project5 use visual format language

--- Project6B/Project6B/ViewController.swift
+++ Project6B/Project6B/ViewController.swift
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
         for label in viewDict.keys {            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewDict))
         }
         // - default is 10,
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewDict))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(==80)]-[label2(==80)]-[label3(==80)]-[label4(==80)]-[label5(==80)]-(>=20)-|", options: [], metrics: nil, views: viewDict))
     }
 
 
