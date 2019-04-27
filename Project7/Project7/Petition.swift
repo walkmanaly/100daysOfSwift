892aa4c916dd8054e1a62f3135109ae8f6f3ceff
Nick <walkmanaly@gmail.com>
2019-04-28 00:46:23 +0800

    day33 project7 part one completed

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Nick on 2019/4/28.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -10,22 +10,39 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    var peredise = [String]()
+    var petitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: url) {
+            if let data = try? Data(contentsOf: url) {
+                //
+                parseData(json: data)
+            }
+        }
+    }
+    
+    func parseData(json: Data) {
+        let jsonDecoder = JSONDecoder()
+        if let petition = try? jsonDecoder.decode(Petitions.self, from: json) {
+            petitions = petition.results
+            tableView.reloadData()
+        }
     }
 }
 
 extension ViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return peredise.count
+        return petitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "text gos here"
-        cell.detailTextLabel?.text = "detail text here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 }
