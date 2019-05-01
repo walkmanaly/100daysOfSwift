f74e2eeed990dedf4246784ded32432679b53aed
Nick <walkmanaly@gmail.com>
2019-05-01 11:00:44 +0800

    day36 project8 set up

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Nick on 2019/5/1.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
