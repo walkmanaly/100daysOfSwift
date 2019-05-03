0bc3029f73a0b0ed04747dfa71691d3792505182
Nick <walkmanaly@gmail.com>
2019-05-03 18:08:55 +0800

    day39 project9 GCD set up

---
+++ Project9/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Nick on 2019/4/28.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
