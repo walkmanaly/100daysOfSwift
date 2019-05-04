bda1e19174af7aa49b83a9bba1e5766898ea72b2
Nick <walkmanaly@gmail.com>
2019-05-04 23:45:18 +0800

    day43 project10 part2

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+    var people = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPersonImage))
     }
 
+    @objc func addPersonImage() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
 
 }
 
 extension ViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -28,7 +38,58 @@ extension ViewController {
             fatalError("can not dequeue cell")
         }
         
+        let person = people[indexPath.item]
+        cell.name.text = person.name
+        let path = getPathComponent().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor.init(white: 0, alpha: 0.5).cgColor
+        cell.imageView.layer.borderWidth = 1
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 8
+        
         return cell
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageString = UUID().uuidString
+        let imagePath = getPathComponent().appendingPathComponent(imageString)
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknow", image: imageString)
+        people.append(person)
+        
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let ac = UIAlertController(title: "Tips", message: "Input name", preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Ok", style: .default) {
+            [weak self, weak ac] _ in
+            guard let name = ac?.textFields?[0].text else { return }
+            if let person = self?.people[indexPath.item] {
+                person.name = name
+            }
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func getPathComponent() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+}
