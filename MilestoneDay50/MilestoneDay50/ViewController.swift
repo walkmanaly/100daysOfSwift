c262854f2262b5b639cd8dbb9733bdf6af95df9c
Nick <walkmanaly@gmail.com>
2019-05-10 00:21:58 +0800

    day50 review project10-12

--- MilestoneDay50/MilestoneDay50/ViewController.swift
+++ MilestoneDay50/MilestoneDay50/ViewController.swift
@@ -8,13 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var images = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPicture))
+        
+        loadData()
     }
 
+    func loadData() {
+        let fm = FileManager.default
+        let bundle = Bundle.main.resourcePath!
+        if let content = try? fm.contentsOfDirectory(atPath: bundle) {
+            for image in content {
+                if image.hasPrefix("nssl") {
+                    images.append(image)
+                }
+            }
+        }
+        
+    }
+    
+    @objc func addNewPicture() {
+        let imgPick = UIImagePickerController()
+        imgPick.allowsEditing = true
+        imgPick.delegate = self
+        imgPick.sourceType = .camera
+        
+        present(imgPick, animated: true)
+    }
 
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = images[indexPath.row]
+        cell.imageView?.image = UIImage(named: images[indexPath.row])
+        
+        return cell
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(type(of: info[.editedImage]))
+        
+        let uuid = UUID().uuidString
+        
+        let path = getPath().appendingPathComponent(uuid)
+        
+        if let image = info[.editedImage] as? UIImage {
+            if let jpegData = image.jpegData(compressionQuality: 0.8) {
+                try? jpegData.write(to: path)
+                images.append(uuid)
+                self.tableView.reloadData()
+            }
+        }
+        
+        dismiss(animated: true)
+        
+    }
+    
+    func getPath() -> URL {
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return url[0]
+    }
+    
 }
 
