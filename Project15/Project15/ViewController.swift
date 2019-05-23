94aa5fd43b3ae994ab416afe82c0f5f5b4657b63
Nick <walkmanaly@gmail.com>
2019-05-23 22:59:34 +0800

    day 57 project15 add animations

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -25,7 +25,24 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
             switch self.currentAnimation {
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                 break
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: 256, y: 256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .orange
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
             default:
                 break
             }
