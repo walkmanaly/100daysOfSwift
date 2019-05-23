021edee7dc5e2c9a900b82f080f578d1f84528d0
Nick <walkmanaly@gmail.com>
2019-05-23 22:43:25 +0800

    day 57 project15 prepare work

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
-
+    @IBAction func Click(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }) { (finished) in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
