bf489694a04bb7ca29d292bc7c41c1c8fc28c09f
Nick <walkmanaly@gmail.com>
2019-05-24 23:40:27 +0800

    day59 review project13-15

---
+++ Day59Review/Day59Review/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  Day59Review
+//
+//  Created by Nick on 2019/5/24.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    var capitalCity: String
+    var size: Double
+    var population: Int
+}
---
+++ Day59Review/Day59Review/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day59Review
+//
+//  Created by Nick on 2019/5/24.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
