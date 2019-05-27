87e66017c775b3218963518857d5e8cc94201080
Nick <walkmanaly@gmail.com>
2019-05-28 00:19:43 +0800

    day62 project17 part one init

---
+++ Project17/Project17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project17
+//
+//  Created by Nick on 2019/5/27.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
