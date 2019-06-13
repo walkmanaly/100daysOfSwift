7e86fbbd3c34f125b3eb393dde6a8419fcfa0dbf
Nick <walkmanaly@gmail.com>
2019-06-14 00:33:13 +0800

    list of pictures

--- swiftUIDemo/swiftUIDemo/ContentView.swift
+++ swiftUIDemo/swiftUIDemo/ContentView.swift
@@ -7,14 +7,34 @@
 //
 
 import SwiftUI
+import Combine
+
+class Datasource: BindableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+    var pictures = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        if let path = Bundle.main.resourcePath, let items = try? fm.contentsOfDirectory(atPath: path) {
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    pictures.append(item)
+                }
+            }
+        }
+        
+        didChange.send(())
+    }
+    
+}
 
 struct ContentView : View {
+    @ObjectBinding var datasource = Datasource()
+    
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
+            List(datasource.pictures.identified(by: \.self)) { picture in
+                Text(picture)
             }.navigationBarTitle(Text("List"))
         }
     }
