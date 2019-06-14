520898075615cd078e52bae17765342b422e11d3
Nick <walkmanaly@gmail.com>
2019-06-14 23:41:41 +0800

    init swiftUIDemo2

---
+++ swiftUIDemo2/swiftUIDemo2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftUIDemo2
+//
+//  Created by Nick on 2019/6/14.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
